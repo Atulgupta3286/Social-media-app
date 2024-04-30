9e3fb26b8f990a7b95c1f6fc3cae6dd5cf318031
Atul Gupta <princegupta9302@gmail.com>
2024-04-30 19:58:43 +0530

    first-commit 29-april home-page-frontend

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+module.exports = router;
